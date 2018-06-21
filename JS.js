cc48983c3ecd4947850aae7f262b579b66516d14
EsmiraldaZ <32571297+EsmiraldaZ@users.noreply.github.com>
2018-06-21 16:13:34 +0300

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,21 @@
+﻿
+function quicksort(array) {
+   if (array.length <= 1) {
+	   return array;
+  }
+
+  var opora = array[0];
+  var left = []; 
+  var right = [];
+
+  for (var i = 1; i < array.length; i++) {
+    array[i] < opora ? left.push(array[i]) : right.push(array[i]);
+  }
+
+  return quicksort(left).concat(opora, quicksort(right));
+};
+
+var myMassiv = [77, 18, 85, 8, 33, 97, 22];
+var sorted = quicksort(myMassiv);
+
+console.log('Sorted array', sorted);
